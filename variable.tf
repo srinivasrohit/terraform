43a0a645d2c018839854b464f332eb9672f09bc2
srinivas <srinivassaripilli321@gmail.com>
2022-05-29 21:11:07 +0530

    added newfile variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+
+
+
+variable "availability_zone_names" {
+  type    = list(string)
+  default = ["us-west-1a"]
+}
+
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8300
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}
+
